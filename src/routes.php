9f2bd2253ca4335a347011abbd5fec2ed9acbb81
Christoff Visser <christoffvis@gmail.com>
2021-02-02 07:16:45 +0000

    Added better documentation to php modules

--- src/routes.php
+++ src/routes.php
@@ -8,20 +8,19 @@ use Illuminate\Http\Request;
 
 Route::group( [ 'prefix' => 'sdnixp' ], function() {
     Route::get(     '',                     'SDNIXPController@index'                )->name( 'sdnixp@index' );
-    Route::get(     'generateConfig',       'SDNIXPController@generateConfig'       )->name( 'sdnixp@generateConfig' );
+    Route::get(     'generateConfig',       'SDNIXPController@runAthos'             )->name( 'sdnixp@runAthos' );
     Route::get(     'getFaucetYaml',        'SDNIXPController@getFaucetYaml'        )->name( 'sdnixp@getFaucetYaml' );
     Route::get(     'getTopologyJson',      'SDNIXPController@getTopologyJson'      )->name( 'sdnixp@getTopologyJson' );
     Route::get(     'getLatestLogs',        'SDNIXPController@getLatestLogs'        )->name( 'sdnixp@getLatestLogs' );
-    Route::get(     'MxGraph',              'SDNIXPController@MxGraph'              )->name( 'sdnixp@MxGraph' );
     Route::get(     'getXML',               'SDNIXPController@getXML'               )->name( 'sdnixp@getXML' );
-    Route::post(    'saveXML',              'SDNIXPController@saveXML'              )->name( 'sdnixp@saveXML' );
-    Route::post(    'saveFaucet',           'SDNIXPController@saveFaucet'           )->name( 'sdnixp@saveFaucet' );
-    Route::post(    'saveTopo',             'SDNIXPController@saveTopo'             )->name( 'sdnixp@saveTopo' );
     Route::get(     'dashboard',            'SDNIXPController@dashboard'            )->name( 'sdnixp@dashboard');
     Route::get(     'testConfigWithOutput', 'SDNIXPController@testConfigWithOutput' )->name( 'sdnixp@testConfigWithOutput');
     Route::get(     'Miru',                 'SDNIXPController@Miru'                 )->name( 'sdnixp@Miru' );
     Route::get(     'miru',                 'SDNIXPController@Miru'                 )->name( 'sdnixp@Miru' );
     Route::get(     'getOF',                'SDNIXPController@getOF'                )->name( 'sdnixp@getOF' );
     Route::get(     'deploy',               'SDNIXPController@deploy'               )->name( 'sdnixp@deploy' );
+    Route::post(    'saveXML',              'SDNIXPController@saveXML'              )->name( 'sdnixp@saveXML' );
+    Route::post(    'saveFaucet',           'SDNIXPController@saveFaucet'           )->name( 'sdnixp@saveFaucet' );
+    Route::post(    'saveTopo',             'SDNIXPController@saveTopo'             )->name( 'sdnixp@saveTopo' );
 });
 
