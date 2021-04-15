06dfa735de6770a8760b2cf16691150bde02ff86
Chris Visser <44282262+Belthazaar@users.noreply.github.com>
2021-04-15 11:46:31 +0900

    Ipv6 fixes and dialogue readablitity (#6)
    * Fixed issue where ip addresses where added as undefined
    
    * Made dialogs more readable

--- src/js/mxgraph/js/Actions.js
+++ src/js/mxgraph/js/Actions.js
@@ -77,7 +77,7 @@ Actions.prototype.init = function()
 	this.addAction('editDiagram...', function()
 	{
 		var dlg = new EditDiagramDialog(ui);
-		ui.showDialog(dlg.container, 620, 420, true, false);
+		ui.showDialog(dlg.container, 730, 500, true, false);
 		dlg.init();
 	});
 	this.addAction('pageSetup...', function() { ui.showDialog(new PageSetupDialog(ui).container, 320, 220, true, true); }).isEnabled = isGraphEnabled;
@@ -92,12 +92,12 @@ Actions.prototype.init = function()
 	this.addAction('tester', function() {new docker(ui).tester()});
 	this.addAction('testeroutput', function() {
 		var dlg = new ShowOutputDialog(ui, "Confirm");
-		ui.showDialog(dlg.container, 800, 575, true, false, null, false);
+		ui.showDialog(dlg.container, 900, 625, true, false, null, false);
 		dlg.init()
 	});
 	this.addAction('generateRun', function() {
 		var dlg = new ShowOutputDialog(ui, "Confirm", true);
-		ui.showDialog(dlg.container, 800, 575, true, false, null, false);
+		ui.showDialog(dlg.container, 900, 625, true, false, null, false);
 		dlg.init()
 	});
 
