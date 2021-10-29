a80fc7624291d5238e3b37adfca251461ff27e79
Christoff Visser <christoffvis@gmail.com>
2021-10-29 17:19:52 +0900

    Added support for the Cerberus controller

--- src/js/mxgraph/js/miru/docker.js
+++ src/js/mxgraph/js/miru/docker.js
@@ -73,7 +73,7 @@ docker.prototype.testerOutput = function (textarea, btns) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             console.log("Complete = " + xhr.responseText);
-            me.addButtons(btns);
+            me.addCerberusButtons(btns);
         }
     };
     xhr.send();
@@ -132,6 +132,85 @@ docker.prototype.addButtons = function (btns) {
         btns.insertBefore(deployButton, btns.firstChild);
     }
 };
+
+
+/**
+ * Adds the required buttons to deploy to Cerberus
+ * @param {Array} btns  - Array of btns to which the new buttons are to be added to
+ */
+ docker.prototype.addCerberusButtons = function (btns) {
+
+    var getTopologyConfig = this.createButton("Download Topology json", this.getTopologyConfig);
+    btns.insertBefore(getTopologyConfig, btns.firstChild);
+    var getLogbtn = this.createButton("Download logs", this.getLogs);
+    btns.insertBefore(getLogbtn, btns.firstChild);
+    var pushConfBtn = this.createButton("Deploy to production", this.pushNewConfig);
+    btns.insertBefore(pushConfBtn, btns.firstChild);
+};
+
+
+/**
+ * Pulls the topology that was sent to Athos and pushes it through to the server
+ */
+docker.prototype.pushNewConfig = function() {
+    let url = window.location.origin + "/miru/getTopologyJson";
+    $.ajax(url)
+        .done(function (topology) {
+            let phpurl = window.location.origin + "/miru/pushCerberusConfig";
+
+            datastring = String(topology);
+            // console.log(`The topology as datastring:\n ${datastring}`)
+            let textArea = document.getElementById("testOutput");
+            textArea.innerHTML = "";
+            var oldVal = "Starting deploy process.\nPlease wait...\n";
+            var newVal = "";
+            var t = document.createTextNode(oldVal);
+            textArea.append(t);
+            let me = this;
+            $.ajax({
+                url: phpurl,
+                type: "POST",
+                data: {"msg": datastring}
+            }).done(function(msg){
+                let textArea = document.getElementById("testOutput");
+                // TODO: Clean up output that is returned from Cerberus
+                var textNode = document.createTextNode(JSON.stringify(msg));
+                textArea.append(textNode);
+                }
+            )
+            .fail(function(msg){
+                let textArea = document.getElementById("testOutput");
+                let textNode = document.createTextNode(`Config failed to be deployed.\nThe following was sent from cerberus:\n ${msg}`)
+                textArea.append(textNode);
+            })
+        })
+        .fail(function () {
+            alert("Something went wrong");
+    });
+}
+
+
+docker.prototype.rollbackCerberusConfiguration = function() {
+    let url = window.location.origin + "/miru/rollbackCerberusConfig";
+
+    $.ajax(url)
+        .done(function (response){
+            let textArea = document.getElementById("testOutput");
+                var textNode = document.createTextNode(JSON.stringify(response));
+                textArea.append(textNode);
+        })
+        .fail(function (response){
+            let textArea = document.getElementById("testOutput");
+            err_msg = (`Config failed to be deployed.\n The following was sent from cerberus: ${response}`);
+            if (textArea) {
+                let textNode = document.createTextNode(err_msg)
+            } else {
+                console.log(err_msg);
+            }
+        })
+
+}
+
 
 /**
  * Retrieves the latest faucet Config that was generated
